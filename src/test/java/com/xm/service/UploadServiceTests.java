7fe3619e8d97b8eda1584fa344f781556018eb1c
Tomek <t.k.zawadzki@gmail.com>
2024-08-04 10:42:17 +0200

    refactoring + not allowed, test

--- src/test/java/com/xm/service/UploadServiceTests.java
+++ src/test/java/com/xm/service/UploadServiceTests.java
@@ -1,11 +1,16 @@
 package com.xm.service;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import com.xm.entity.CryptoNotAllowed;
+import com.xm.exception.XMException;
+import com.xm.repository.CryptoNotAllowedRepository;
 import com.xm.repository.SymbolRepository;
 import com.xm.repository.TickRepository;
 
@@ -21,6 +26,9 @@ class UploadServiceTests {
     @Autowired
     private TickRepository tickRepository;
 
+    @Autowired
+    private CryptoNotAllowedRepository cryptoNotAllowedRepository;
+
     @Test
     void testUpload() {
         uploadService.uploadFile(getClass().getClassLoader().getResourceAsStream("files/BTC_values.csv"));
@@ -31,6 +39,17 @@ class UploadServiceTests {
 
         assertThat(symbolRepository.count()).isEqualTo(5);
         assertThat(tickRepository.count()).isEqualTo(450);
+    }
+
+    @Test
+    void testNotAllowed() {
+        cryptoNotAllowedRepository.save(CryptoNotAllowed.builder().name("BTC").build());
+
+        Exception exception = assertThrows(XMException.class, () -> {
+            uploadService.uploadFile(getClass().getClassLoader().getResourceAsStream("files/BTC_values.csv"));
+        });
+
+        assertTrue(exception.getMessage().contains("Crypto not allowed: BTC"));
     }
 
     // TODO add more test with uploading same file, inconsistent data, changed columns etc
